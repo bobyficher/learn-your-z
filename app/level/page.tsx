64510e04451855b634fdf50e1e5f48ae70ddac8a
Boby Ficher <bobyficsher14@gmail.com>
2025-07-08 14:11:08 +0530

    fix: resolve useSearchParams error

--- app/level/page.tsx
+++ app/level/page.tsx
@@ -1,10 +1,15 @@
 "use client"
-import { useSearchParams } from 'next/navigation'
 import { levels } from '@/src/data/levels';
+import { useEffect } from 'react';
 
 export default function LevelDetail() {
-  const searchParams = useSearchParams()
-  const id = searchParams.get('id')
+  let id = '';
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    id = urlParams.get('id') ?? "";
+  }, []); // Empty dependency array ensures it runs once after mount
+
   const level = levels.find((l) => l.id === parseInt(id as string));
 
   if (!level) {
@@ -12,26 +17,26 @@ export default function LevelDetail() {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">{level.title}</h1>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Challenge</h2>
-        <p>{level.challenge}</p>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-6">{level.title}</h1>
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Challenge</h2>
+          <p>{level.challenge}</p>
+        </div>
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Explanation</h2>
+          <p>{level.explanation}</p>
+        </div>
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Git URL</h2>
+          <a href={level.gitUrl} className="text-blue-500">{level.gitUrl}</a>
+        </div>
+        <button
+          onClick={() => location.href = "/home"}
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+        >
+          Back to Dashboard
+        </button>
       </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Explanation</h2>
-        <p>{level.explanation}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Git URL</h2>
-        <a href={level.gitUrl} className="text-blue-500">{level.gitUrl}</a>
-      </div>
-      <button
-        onClick={() => location.href = "/home"}
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-      >
-        Back to Dashboard
-      </button>
-    </div>
   );
 }
